a5545489b3949703d70efd944d52d64bf13b48ea
Bryan Wandera <emmabryanwandera@gmail.com>
2021-09-29 20:52:20 +0300

    Edited settings

--- src/personalwebsite/settings.py
+++ src/personalwebsite/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-django_heroku.settings(localsl())
+django_heroku.settings(locals())
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
